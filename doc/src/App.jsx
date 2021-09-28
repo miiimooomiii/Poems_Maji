649f55e99e54f1b571d0800195a89f6ea6850845
Mimomi <miiimooomiii@narod.ru>
2021-09-28 22:44:31 +0900

    add doc

---
+++ doc/src/App.jsx
@@ -0,0 +1,25 @@
+import logo from "./logo.svg";
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div class={styles.App}>
+      <header class={styles.header}>
+        <img src={logo} class={styles.logo} alt="logo" />
+      </header>
+      <div>
+        <p>あのね</p>
+        <a
+          class={styles.link}
+          href="https://github.com/solidjs/solid"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Solid
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
