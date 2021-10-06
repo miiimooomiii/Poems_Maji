2ed32ec2b685c2e661bb6bf318fe8fc7ccda6ca0
Mimomi <miiimooomiii@narod.ru>
2021-10-06 16:26:23 +0900

    fix

--- form/src/App.jsx
+++ form/src/App.jsx
@@ -3,7 +3,6 @@ import { Blob, Blobscroll, Thank } from './action';
 
 import styles from './App.module.css';
 
-
 import Logo from './logo.svg';
 
 
@@ -16,21 +15,20 @@ function App() {
           </button>
           <img src={Logo} class={styles.logo} id='logo' alt='logo' />
       </header>
-      <div class={styles.headmargin} />
       <appbody id='appbody'>
         <form id='form' class={styles.form} class={styles.invisible} action='https://docs.google.com/forms/u/0/d/e/1FAIpQLSd03E7BPIpR0Y9E-vfnlUTf2eU4J3OGripRMN_YrJGthUE89w/formResponse' method="post" target="hidden_iframe" onsubmit={Thank}>
-          <iframe name="hidden_iframe" id="hidden_iframe" style="display:none;"></iframe>
+          <iframe name="hidden_iframe" id="hidden_iframe" style="display:none;" />
            <label class={styles.column} for='title'>題名
-            <p class={styles.subinfo}>空欄の場合は『無題』となります</p>
-            <input type='text' id='title' name='entry.1866521228' placeholder="『無題』"></input>
+            <p class={styles.subinfo}>空欄は『無題』となります</p>
+            <input type='text' id='title' name='entry.1866521228' placeholder="『無題』" />
           </label>
           <label class={styles.column} for='poem'>本文
             <p class={styles.subinfo} class={styles.required}>必須</p>
-            <textarea id='poem' name='entry.1602729063' required=""></textarea>
+            <textarea id='poem' name='entry.1602729063' required="" />
           </label>
           <label class={styles.column} for='name'>著者名
-            <p class={styles.subinfo}>空欄の場合は『匿名』となります</p>
-            <input type='text' id='name' name='entry.1285093387' placeholder="『匿名』"></input>
+            <p class={styles.subinfo}>空欄は『匿名』となります</p>
+            <input type='text' id='name' name='entry.1285093387' placeholder="『匿名』" />
           </label>
           <label class={styles.column} for='addition'>付記
             <p class={styles.subinfo}>解説や補足などはこちらへ</p>
@@ -38,12 +36,17 @@ function App() {
           </label>
           <label class={styles.column} for='link'>リンク
             <p class={styles.subinfo}>SNSやサイトのURLはこちらへ</p>
-            <input type='url' id='link' name='entry.272121178' placeholder="https://"></input>
+            <input type='url' id='link' name='entry.272121178' placeholder="https://" />
           </label>
-          <p><button type="submit" class={styles.submit} name="button" value="送信">送信</button></p>
+          <label class={styles.column} for='check'>
+            <input id='check' type='checkbox' required></input>
+            <p class={styles.subinfo} class={styles.required} style='top:0.96em;left:7.5em'>必須</p>
+            <p class={styles.agreement}>利用規約に同意します。</p>
+          </label>
+          <button type='submit' class={styles.submit} name="button" value="送信">送信</button>
         </form>
       </appbody>
-      <thankblob class={styles.invisible} id='thankblob'>
+       <thankblob class={styles.invisible} id='thankblob'>
         <div class={styles.blob} style='background-color:#f4f4f4; pointer-events:none;'>
           <p class={styles.thankmessage}>ありがとうございます。</p>
         </div>
--- form/src/index.jsx
+++ form/src/index.jsx
@@ -1,13 +1,14 @@
-import { render } from "solid-js/web";
+import { render } from 'solid-js/web';
 
-import "./index.css";
-import App from "./App";
+import './index.css';
+import App from './App';
 import styles from './App.module.css';
 
 render(App, document.getElementById("root"));
 
-const txHeight = 135;
+const txHeight = 108;
 const tx = document.getElementsByTagName('textarea');
+const cb = document.getElementById('check');
 const rq = document.getElementsByClassName(styles.required);
 
 function OnInput() {
@@ -27,10 +28,18 @@ function OnInputRequired() {
   }
 }
 
+function OnInputRequired1() {
+  if (this.checked) {
+    rq[1].classList.add(styles.filled);
+  } else {
+    rq[1].classList.remove(styles.filled);
+  }
+}
+
 tx[0].addEventListener('input', OnInputRequired, false);
+cb.addEventListener('input', OnInputRequired1, false);
 
 for (let i = 0; i < tx.length; i++) {
-  console.log(tx[i].scrollHeight);
   if (tx[i].value == '') {
     tx[i].setAttribute('style', 'height:' + txHeight + 'px;overflow-y:hidden;');
   } else {
